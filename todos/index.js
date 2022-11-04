e44675232bc807bfc46835b4cbfc68d1e2e72080
Michael Nash <michael.nash@kin.com>
2022-11-04 17:17:13 -0400

    created quit menu command

--- todos/index.js
+++ todos/index.js
@@ -16,7 +16,13 @@ const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            { label: 'New Todo' }
+            { label: 'New Todo' },
+            {
+                label: 'Quit',
+                click() {
+                    app.quit()
+                }
+            }
         ]
     }
 ]
